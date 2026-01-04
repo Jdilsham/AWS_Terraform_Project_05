83ab74b408eb722fce312ea11d91c2d1d7951495
Jdilsham <janithadilsham@gmail.com>
2026-01-04 23:09:05 +0530

    add vpc module

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
 provider "aws" {
   region = var.aws_region
 }
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.5.1"
+
+  name = var.project_name
+  cidr = var.vpc_cidr
+
+  azs            = [var.aws_region]
+  public_subnets = [var.public_subnet_cidrs]
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Project = var.project_name
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,34 +1,34 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1a"
 }
 
 variable "project_name" {
   description = "Project name for tagging"
-  type = string
-  default = "terraform-webapp"
+  type        = string
+  default     = "terraform-webapp"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
-  type = string
-  default = "10.0.0.0/16"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
   description = "List of CIDR blocks for public subnets"
-  type = string
-  default = "10.0.1.0/24"
+  type        = string
+  default     = "10.0.1.0/24"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
-  type = string
-  default = "t2.micro"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "key_pair_name" {
   description = "Name of the existing key pair to use for EC2 instances"
-  type = string
+  type        = string
 }
